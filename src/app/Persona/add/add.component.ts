c5dd2f6336223d5e01c35a94232d3e5af4aeee21
FrankCQ12345678 <franklincuroquispe@gmail.com>
2022-12-15 22:18:15 -0500

    primer commit

---
+++ src/app/Persona/add/add.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/Service/service.service';
+import { Persona } from 'src/app/Modelo/Persona';
+
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit{
+
+  constructor(private router:Router, private service:ServiceService){}
+
+  ngOnInit() {
+    
+  }
+  Guardar(persona:Persona){
+    this.service.createPersona(persona)
+    .subscribe(data=>{
+      alert("Se agrego con Exito...¡¡¡");
+      this.router.navigate(["listar"]);
+    })
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core'
+
+import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/add/add.component';
+import { EditComponent } from './Persona/edit/edit.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceService } from './Service/service.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarComponent,
+    AddComponent,
+    EditComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [ServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
